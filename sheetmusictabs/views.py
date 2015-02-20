269bd0feb8d66d7ada15d0f4bf220e7c17cc9ac9
Bill <beiller@gmail.com>
2015-02-20 02:33:29 -0500

    tab page proper and various fixes

--- sheetmusictabs/views.py
+++ sheetmusictabs/views.py
@@ -2,6 +2,7 @@ from sheetmusictabs.models import Tabs, Comment
 from django.http import Http404, HttpResponse
 from django.shortcuts import render
 import zlib
+import settings
 
 
 def tab_list(request):
@@ -9,7 +10,7 @@ def tab_list(request):
     for tab in tabs:
         if tab.tab is None :
             tab.tab = zlib.decompress(tab.gzip_tab)
-    return render(request, 'tablist.html', {'tabs': tabs})
+    return render(request, 'tablist.html', {'tabs': tabs, 'site_globals': settings.SITE_GLOBALS})
 
 
 def tab_page(request, tab_id):
@@ -22,4 +23,4 @@ def tab_page(request, tab_id):
     comments = Comment.objects.filter(tab_id=tab_id).filter(spam=0)
     if tab.tab is None :
         tab.tab = zlib.decompress(tab.gzip_tab)
-    return render(request, 'tab.html', {'tab': tab, 'comments': comments})
+    return render(request, 'tab.html', {'tab': tab, 'comments': comments, 'site_globals': settings.SITE_GLOBALS})
