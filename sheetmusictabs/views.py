4022dff026e143da6b61088ca4dbde5bd07f2024
itbxh <beiller@gmail.com>
2015-02-25 16:00:50 -0500

    add chord diagrams

--- sheetmusictabs/views.py
+++ sheetmusictabs/views.py
@@ -3,6 +3,7 @@ from django.http import Http404, HttpResponse
 from django.shortcuts import render
 import zlib
 import settings
+import re
 
 
 def tab_list(request):
@@ -13,6 +14,24 @@ def tab_list(request):
     return render(request, 'tablist.html', {'tabs': tabs, 'site_globals': settings.SITE_GLOBALS})
 
 
+def parse_chords_list(text_input):
+    chord_expression = (
+        '\s([a-g])([#b])?\s?(min|maj|m|dim|5|6|7|maj7|9|maj9|11|13|maj13|min6|min7|'
+        'min9|min11|min13|sus2|sus4|dim|aug|6/9|7sus4|7b5|7b9|9sus4|add9|aug9)?\s'
+    )
+    result = re.findall(chord_expression, text_input, re.IGNORECASE)
+    chords = set()
+    for line in result:
+        if len(line) == 3:
+            #format chord
+            modifier = line[2]
+            if modifier == 'm':
+                modifier = 'min'
+            chord = (line[0].upper() + line[1] + " " + modifier.lower()).strip()
+            chords.add(chord)
+    return chords
+
+
 def tab_page(request, tab_id):
     try:
         tab_id = int(tab_id)
@@ -21,6 +40,14 @@ def tab_page(request, tab_id):
 
     tab = Tabs.objects.get(pk=tab_id)
     comments = Comment.objects.filter(tab_id=tab_id).filter(spam=0)
-    if tab.tab is None :
+    if tab.tab is None:
         tab.tab = zlib.decompress(tab.gzip_tab)
-    return render(request, 'tab.html', {'tab': tab, 'comments': comments, 'site_globals': settings.SITE_GLOBALS})
+    tab.chords = parse_chords_list(tab.tab)
+    tab.band = re.sub(' tabs$', '', tab.band, re.IGNORECASE)
+    tab.name = re.sub('\s+(Tab|Tabs|Chord|Chords)$', '', tab.name, re.IGNORECASE)
+
+    return render(request, 'tab.html', {
+        'tab': tab,
+        'comments': comments,
+        'site_globals': settings.SITE_GLOBALS
+    })
