bf658da2b2791bfcbc4dae6946d6c3df518947ff
Bill <beiller@gmail.com>
2015-02-25 22:11:36 -0500

    update to templates for look and feel

--- sheetmusictabs/views.py
+++ sheetmusictabs/views.py
@@ -36,11 +36,12 @@ def parse_chords_list(text_input):
 def annotate_chords(text_input):
     chord_expression = (
         '(\s)(([a-g])([#b])?\s?(min|maj|m|dim|5|6|7|maj7|9|maj9|11|13|maj13|min6|min7|'
-        'min9|min11|min13|sus2|sus4|dim|aug|6/9|7sus4|7b5|7b9|9sus4|add9|aug9)?)(\s\s)'
+        'min9|min11|min13|sus2|sus4|dim|aug|6/9|7sus4|7b5|7b9|9sus4|add9|aug9)?)'
+        '(,|\s\s)'
     )
     return re.sub(
         chord_expression,
-        r"""\g<1><a href="#" onclick="show_diagram('\g<2>');">\g<2></a>\g<6>""",
+        r"""\g<1><a href="#" class="show_diagram_link">\g<2></a>\g<6>""",
         text_input,
         count=0,
         flags=re.IGNORECASE
@@ -57,7 +58,7 @@ def tab_page(request, tab_id):
     comments = Comment.objects.filter(tab_id=tab_id).filter(spam=0)
     if tab.tab is None:
         tab.tab = zlib.decompress(tab.gzip_tab)
-    tab.chords = parse_chords_list(tab.tab)
+    #tab.chords = parse_chords_list(tab.tab)
     tab.band = re.sub(' tabs$', '', tab.band, re.IGNORECASE)
     tab.name = re.sub('\s+(Tab|Tabs|Chord|Chords)$', '', tab.name, re.IGNORECASE)
     tab.tab = annotate_chords(striptags(tab.tab))
