de0e8722878dacbcc5aaad8dcd5410b135fe0acd
itbxh <beiller@gmail.com>
2015-02-26 13:12:30 -0500

    add rudimentary search

--- sheetmusictabs/urls.py
+++ sheetmusictabs/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^$', sheetmusictabs.views.tab_list),
     #0/bands/B/Bob+Seger+tabs/185010/Turn+The+Page+Tab.html
-    url(r'^bands/[A-Z0-9]/[^/]+/([0-9]+)/[^/]+$', sheetmusictabs.views.tab_page)
+    url(r'^bands/[A-Z0-9]/[^/]+/([0-9]+)/[^/]+$', sheetmusictabs.views.tab_page),
+    url(r'^ajax/search.*$', sheetmusictabs.views.search)
 )
--- sheetmusictabs/views.py
+++ sheetmusictabs/views.py
@@ -1,6 +1,8 @@
 from django.template.defaultfilters import striptags
-from sheetmusictabs.models import Tabs, Comment
-from django.http import Http404, HttpResponse
+from sheetmusictabs.models import Tabs, Comment, TabsFulltext
+from django.http import Http404, HttpResponse, JsonResponse
+from django.db.models import Q
+from urllib import urlencode
 from django.shortcuts import render
 import zlib
 import settings
@@ -68,3 +70,35 @@ def tab_page(request, tab_id):
         'comments': comments,
         'site_globals': settings.SITE_GLOBALS
     })
+
+
+def url_from_id(tab_id):
+    tab = Tabs.objects.get(pk=tab_id)
+    if tab is not None:
+        return url_from_tab(tab)
+    else:
+        return None
+
+
+def url_from_tab(tab):
+    url = '/bands/' + tab.band[0].upper() + '/'
+    url += tab.band.replace(' ', '+')
+    url += '/' + str(tab.id) + '/'
+    url += tab.name.replace(' ', '+') + '.html'
+    return url
+
+
+def search(request):
+    search_string = request.GET.get('q')
+
+    #tabs = TabsFulltext.objects.filter(Q(name__search="+"+search_string) | Q(band__search="+"+search_string))[:20]
+    tabs = TabsFulltext.objects.raw("""
+        SELECT tabs.id, tabs.name, tabs.band
+        FROM tabs_fulltext
+        JOIN tabs ON tabs.id = tabs_fulltext.id
+        WHERE match (tabs_fulltext.name,tabs_fulltext.band) against (%s) LIMIT 20
+    """, [search_string])
+    response = {}
+    for tab in tabs:
+        response[tab.id] = {'name': tab.name, 'band': tab.band, 'url': url_from_tab(tab)}
+    return JsonResponse(response)
