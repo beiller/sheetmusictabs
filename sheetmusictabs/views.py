fa6d0ae21abbc3e48a1241a66d86a8e66ea80f59
Bill <beiller@gmail.com>
2017-08-16 23:39:54 -0400

    clickable search results

--- sheetmusictabs/views.py
+++ sheetmusictabs/views.py
@@ -108,7 +108,7 @@ def detect_spam_by_content(content):
     words = [
         'ugg', 'http', 'href', 'viagra', 'preteen', 'online', 'cialis', 'pharmacy',
         'prescription', 'lolita', 'nude', 'url', 'buy', 'blog', 'content', '.com',
-        'article', 'website', '. and bye.'
+        'article', 'website', '. and bye.', 'bookmark', 'penis', 'download'
     ]
     for word in words:
         if word in to_test.lower():
